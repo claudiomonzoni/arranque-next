84571ef26f9c5369c8e58c6b8e021fd927a87f39
claudiomonzoni <claudiomonzoni@hotmail.com>
2022-06-26 20:29:32 -0500

    primer commit

---
+++ components/Footer.jsx
@@ -0,0 +1,7 @@
+
+export default function Footer() {
+  return (
+    <footer>Soy del clan del pie</footer>
+  )
+}
+
---
+++ components/Header.jsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <nav>
+      <ul>
+        <li>Aqui el logo</li>
+        <li>
+          <Link href={"/seccion-a"}>
+            <a>Seccion A</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
